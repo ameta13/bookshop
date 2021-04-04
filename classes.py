82d58a11f7b77ce0af1509f9693f687542543e10
Maksim Fedoseev <maxim.fedoseev13@gmail.com>
2021-04-04 22:38:47 +0300

    Added some function

--- classes.py
+++ classes.py
@@ -1,8 +1,10 @@
 import random
+from collections import OrderedDict, defaultdict
 from typing import List, Tuple
 
 CURRENT_YEAR = 2021
 
+# TODO: добавить вычисление рейтинга книги
 
 class Book:
     def __init__(self, authors: List[str], name: str, publishing: str, year: int, pages: int, category: str,
@@ -17,21 +19,30 @@ class Book:
         self.rating = rating
         self.category = category
 
+    def __str__(self):
+        return ', '.join(self.authors) + ': ' + self.name
+
+
 class BookCopies:
     def __init__(self, book: Book, count: int):
         self.book = book
         self.count = count
+
 
 class Order:
     def __init__(self, customer: str, contact: str, cart: List[BookCopies]):
         self.customer = customer
         self.contact = contact
         self.cart = cart
+        self.remain_cart = [i for i in range(len(cart))]
+
 
 class OrderPublising:
-    def __init__(self, books_copies: List[BookCopies]):
+    def __init__(self, publishing: str, books_copies: List[BookCopies]):
+        self.publishing = publishing
         self.books_copies = books_copies
         return
+
 
 class Randomizer:
     def __init__(self, delivery_range: Tuple[int], cnt_books: int, books: List[Book]):
@@ -50,61 +61,93 @@ class Randomizer:
                                    ('Moore', 'moore@more.tv'),
                                    ('Taylor', 'bestsinger@gmail.com'),
                                    ('Scott', 'scotttt@yahoo.com'),]
+        self.min_cnt_order = 1  # минимальное кол-во заказов за один шаг
+        self.max_cnt_order = 5  # максимальное кол-во заказов за один шаг
         return
 
-    def rand_delivery_time(self):
+    def rand_delivery_time(self) -> int:
         return random.randint(self.delivery_range[0], self.delivery_range[1])
 
-    def rand_cnt_book_in_order(self):
+    def rand_cnt_book_in_order(self) -> int:
         # случайное количество разных книг в заказе
         return random.randint(1, self.max_diff_book)
 
-    def rand_cnt_copies(self):
+    def rand_cnt_copies(self) -> int:
         # случайное количество копий для книги в заказе
         return random.randint(1, self.max_copies)
 
-    def rand_book(self):
+    def rand_book(self) -> int:
         # случайная книга
         return random.randint(0, self.cnt_books - 1)
 
-    def rand_order(self) -> List[BookCopies]:
+    def rand_order(self) -> Order:
         cnt = self.rand_cnt_book_in_order()
         order = random.sample(self.books, cnt)
         customer, contact = random.choice(self.customer_n_contact)
         cart = [BookCopies(book, self.rand_cnt_copies()) for book in order]
         return Order(customer, contact, cart)
 
+    def rand_cnt_order(self) -> int:
+        return random.randint(self.min_cnt_order, self.max_cnt_order)
+
 
 class BookShop:
-    def __init__(self, start_assort: List[BookCopies], minimum_cnt_for_order: int, margin: int, margin_new_book: int, experiment):
+    def __init__(self, books_assort: List[BookCopies], minimum_cnt_for_order: int, margin: int, margin_new_book: int, experiment):
         self.orders = []
-        self.books = start_assort
+        self.books = books_assort
         self.minimum_cnt_for_order = minimum_cnt_for_order
         self.margin = margin
         self.margin_new_book = margin_new_book
         self.experiment = experiment  # так ли надо?
+        self.auth_name2idx_book = {str(bc.book): idx for idx, bc in enumerate(self.books)}
+        self.min_cnt_order_book = 0  # сколько раз заказали наименее популярную книгу (с наименьшим числом заказов)
+        self.max_cnt_order_book = -1  #сколько раз заказали наиболее популярную книгу (с наибольшим числом заказов)
         return
 
     def receive_order(self, new_order: Order):
-        pass
-
-    def create_order_publishing(self, books_copies: BookCopies):
-        pass
+        # TODO: добавить вычисление рейтинга книги
+        self.orders.append(new_order)
+        return
 
     def try_complete_orders(self):
-        # можно ли частично выполнять заказы? Или сначала в магазине должны собраться все книги и только потом сразу все книги покупатель купит?
+        """
+        если из заказа в наличии есть книга в нужном кол-ве, то она откладывается из ассортимента (выкидываем из remain_cart)
+            И составляется заказ в издательство на self.minimum_cnt_for_order, если после вычитания у нас осталось "< minimum_cnt_for_order" книг
+        Если из заказа нет нужного кол-ва определенной книги (надо N книг), то в издательство заказывается N книг
+        """
+        order_publ = {}
         for order in self.orders:
-            pass
-        # используем self.experiment.update_done_orders_publishing и self.experiment.update_done_orders
+            for i, in order.remain_cart:
+                bc = order.cart[i]
+                idx_book_in_assort = self.auth_name2idx_book[str(bc.book)]
+                book = self.books[idx_book_in_assort].book
+                if bc.count <= self.books[idx_book_in_assort].count:
+                    self.books[idx_book_in_assort].count -= bc.count
+                    order.remain_cart.pop(i)
+                    cnt_in_assort = self.books[idx_book_in_assort].count
+                    if cnt_in_assort < self.minimum_cnt_for_order:
+                        if book.publishing not in order_publ:
+                            order_publ[book.publishing] = defaultdict(int)
+                        order_publ[book.publishing][book] += self.minimum_cnt_for_order
+                else:
+                    if book.publishing not in order_publ:
+                        order_publ[book.publishing] = defaultdict(int)
+                    order_publ[book.publishing][book] += bc.count
+            if len(order.remain_cart) == 0:
+                self.experiment.update_done_order(order)
+        for publ, orders_to_publ in order_publ.items():
+            ord_publ = OrderPublising(publ, [BookCopies(book, cnt) for book, cnt in orders_to_publ.items()])
+            self.experiment.add_order_publishing(ord_publ)
+        return
 
-    def get_books_from_publishing(self, orders: OrderPublising):
+    def get_books_from_publishing(self, orders: List[OrderPublising]):
         new_books_copies = [book_copies for order in orders for book_copies in order.books_copies]
         for new_book_copies in new_books_copies:
             id_book = [id for id, book_copies in enumerate(self.books) if book_copies.book == new_book_copies.book]
             assert len(id_book) == 1, f'BookShop:: get_books_from_publishing(): found {len(id_book)} identical book!'
             id_book = id_book[0]
             self.books[id_book].count += new_book_copies.count
-        self.try_complete_orders()
+
 
 class Experiment:
     def __init__(self, simulation_period: int, simulation_step: int, books: List[Book], start_assort: List[int],
@@ -119,17 +162,39 @@ class Experiment:
         assert len(start_assort) == len(books), f"Wrong length of 'start_assort' = {len(start_assort)}, expected: {len(books)}"
         for book in books:
             book.margin = margin_new_book if book.year == CURRENT_YEAR else margin
-        self.start_assort = [BookCopies(book, cnt) for book, cnt in zip(books, start_assort)]
+        self.start_book_assort = [BookCopies(book, cnt) for book, cnt in zip(books, start_assort)]
         self.orders_publishing = []
         self.done_orders = []
         self.done_orders_publishing = []
         self.randomizer = Randomizer(delivery_range, len(books), books)
         self.book_shop = None  # сам магазин (класс BookShop)
+
+        categories = set([b.category for b in books])
+        sell_statistic = {cat: 0 for cat in categories}
+        self.sell_statistic = OrderedDict(sorted(sell_statistic.items()))
+        self.cnt_steps = 0
         return
 
     def start(self):
-        self.book_shop = BookShop(self.start_assort, self.minimum_cnt_for_order, self.margin, self.margin_new_book, self)
+        self.book_shop = BookShop(self.start_book_assort, self.minimum_cnt_for_order, self.margin, self.margin_new_book, self)
         # дописать
+
+    def make_one_step(self):
+        cnt_orders = self.randomizer.rand_cnt_order()
+        for i in range(cnt_orders):
+            self.create_order()
+        self.check_orders_publising()
+        self.book_shop.try_complete_orders()
+        return
+
+    def make_step(self):
+        for i in range(self.simulation_step):
+            self.make_one_step()
+        self.cnt_steps += self.simulation_step
+
+    def make_all_steps(self):
+        while self.cnt_steps < self.simulation_period:
+            self.make_step()
 
     def create_order(self):
         new_order = self.randomizer.rand_order()
@@ -143,14 +208,14 @@ class Experiment:
 
     def check_orders_publising(self):
         done_orders = [order for time, order in self.orders_publishing if time == 0]
-        self.done_orders_publishing += done_orders
-        self.book_shop.get_books_from_publishing(done_orders)
+        if len(done_orders) > 0:
+            self.done_orders_publishing += done_orders
+            self.orders_publishing = [x for x in self.orders_publishing if x[0] > 0]
+            self.book_shop.get_books_from_publishing(done_orders)
         return
 
-    def update_done_orders_publishing(self, new_done_orders_publ):
-        self.done_orders_publishing += new_done_orders_publ
-        return
-
-    def update_done_orders(self, new_done_orders):
-        self.done_orders += new_done_orders
+    def update_done_order(self, new_done_order: Order):
+        self.done_orders.append(new_done_order)
+        for bc in new_done_order.cart:
+            self.sell_statistic[bc.book.category] += bc.count
         return
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from classes import *
 import random
+
 MIN_GEN_CNT, MAX_GEN_CNT = 4, 8
 
 books = [Book(['Михаил Булгаков'], 'Мастер и Маргарита', 'Юнацтва Минск', 1966, 670, 'Художественная лит-ра', 500),
@@ -10,43 +11,102 @@ books = [Book(['Михаил Булгаков'], 'Мастер и Маргари
          Book(['Теодор Драйзер'], 'Американская трагедия', 'АСТ', 2021, 960, 'Художественная лит-ра', 400),
          Book(['Ильин В.А.', 'Садовничий В.А.', 'Сендов Бл.Х.'], 'Математический анализ', 'Юрайт', 2019, 648, 'Учебная лит-ра', 740),
          Book(['Александров Н.В.', 'Яшкин А.Я.'], 'Курс общей физики. Механика', 'Юрайт', 1978, 416, 'Учебная лит-ра', 630),
-         Book(['Бьярне Страуструп'], 'Программирование на C++', 'Вильямс', 2018, 1328, 'Учебная лит-ра', 1500),]
+         Book(['Бьярне Страуструп'], 'Программирование на C++', 'Вильямс', 2018, 1328, 'Учебная лит-ра', 1500), ]
 
 DEFAULT_FONT = ('Arial', 10)
-left_col1 = [[sg.Text('Launch parameters', font=DEFAULT_FONT)],
-            [sg.Text('Simulation period (days)', font=DEFAULT_FONT), sg.InputText('15', font=DEFAULT_FONT, size=(3, 1))],
-            [sg.Text('Simulation step (days)', font=DEFAULT_FONT), sg.InputText('3', font=DEFAULT_FONT, size=(3, 1))],
-            [sg.Text('Delivery range (days)', font=DEFAULT_FONT), sg.InputText('2-4', font=DEFAULT_FONT, size=(3, 1))],
-            [sg.Text('Margin (%)', font=DEFAULT_FONT), sg.InputText('20', font=DEFAULT_FONT, size=(3, 1))],
-            [sg.Text('Margin for new books (%)', font=DEFAULT_FONT), sg.InputText('30', font=DEFAULT_FONT, size=(3, 1))],
-            [sg.Text('Min count for order book', font=DEFAULT_FONT), sg.InputText('5', font=DEFAULT_FONT, size=(3, 1))],
-            [sg.Button('Start', font=DEFAULT_FONT), sg.Button('Exit', font=DEFAULT_FONT)]]
-cnt_parameters = len(left_col1) - 2  # -2  -- 'Launch parameters' and row with buttons
-right_col1 = [[sg.Text('Starting counts of each book',)]]
+left_col1 = [
+    [sg.Text('Simulation period (days)', font=DEFAULT_FONT), sg.InputText('15', font=DEFAULT_FONT, size=(3, 1))],
+    [sg.Text('Simulation step (days)', font=DEFAULT_FONT), sg.InputText('3', font=DEFAULT_FONT, size=(3, 1))],
+    [sg.Text('Delivery range (days)', font=DEFAULT_FONT), sg.InputText('2-4', font=DEFAULT_FONT, size=(3, 1))],
+    [sg.Text('Margin (%)', font=DEFAULT_FONT), sg.InputText('20', font=DEFAULT_FONT, size=(3, 1))],
+    [sg.Text('Margin for new books (%)', font=DEFAULT_FONT), sg.InputText('30', font=DEFAULT_FONT, size=(3, 1))],
+    [sg.Text('Min count for order book', font=DEFAULT_FONT), sg.InputText('5', font=DEFAULT_FONT, size=(3, 1))],
+    [sg.Button('Start', font=DEFAULT_FONT), sg.Button('Exit', font=DEFAULT_FONT)]]
+cnt_parameters = len(left_col1) - 1  # -1  -- row with buttons
+headers = [sg.Text('Launch parameters', font=DEFAULT_FONT, size=(33, 1), justification='c'),
+           sg.Text('Starting counts of each book', font=DEFAULT_FONT, size=(90, 1), justification='c')]
+right_col1 = []
 for book in books:
-    text = ', '.join(book.authors) + ': ' + book.name
-    right_col1.append([sg.Text(text, font=(DEFAULT_FONT[0], DEFAULT_FONT[1] if len(text) < 55 else DEFAULT_FONT[1]-2)),
-                      sg.Input(str(random.randint(MIN_GEN_CNT, MAX_GEN_CNT)), font=DEFAULT_FONT, size=(3, 1))])
-layout1 = [[sg.Column(left_col1, element_justification='r'), sg.Column(right_col1, element_justification='r')]]
+    text = str(book)
+    right_col1.append(
+        [sg.Text(text, font=(DEFAULT_FONT[0], DEFAULT_FONT[1] if len(text) < 55 else DEFAULT_FONT[1] - 2)),
+         sg.Input(str(random.randint(MIN_GEN_CNT, MAX_GEN_CNT)), font=DEFAULT_FONT, size=(3, 1))])
+
+layout1 = [headers,
+           [sg.Column(left_col1, element_justification='r'),
+            sg.Column(right_col1, element_justification='r', size=(650, 160), scrollable=True)]]
 
 window1 = sg.Window('Book shop. Input parameters.', layout1)
+def create_col(name_col, arg):
+    global DEFAULT_FONT
+    widths = {'Автор': 40, 'Название': 30, 'Кол-во': 12, 'Покупатель': 20, '#': 2, 'Тематика': 25, 'Издат.': 12,
+              'Рейтинг': 14}
+    num_rows = 7
+    justification = 'l'
+    if name_col == 'Ассортимент':  # arg - List[BookCopies]
+        headings = ['Автор', 'Название', '#']
+        data = [[', '.join(bc.book.authors), bc.book.name, bc.count] for bc in arg]
+    elif name_col == 'Текущие заказы' or name_col == 'Выполн. заказы':  # arg - List[Order]
+        headings = ['Покупатель', 'Автор', 'Название', '#']
+        data = [[orde.customer, '\n'.join([', '.join(bc.book.authors) for bc in orde.cart]),
+                 '\n'.join([bc.book.name for bc in orde.cart]), '\n'.join([str(bc.count) for bc in orde.cart])] for orde in arg]
+    elif name_col == 'Продажи':  # arg - OrderedDict[(category, cnt)]
+        headings = ['Тематика', '#']
+        data = [[cat, cnt] for cat, cnt in arg.items()]
+    elif name_col == 'Выполн. заявки в издат.':  # arg - List[OrderPublishing]
+        headings = ['Издат.', 'Автор', 'Название', '#']
+        data = [[orde.publishing, '\n'.join([', '.join(bc.book.authors) for bc in orde.books_copies]),
+                 '\n'.join([bc.book.name for bc in orde.books_copies]), '\n'.join([str(bc.count) for bc in orde.books_copies])] for orde in arg]
+    elif name_col == 'Топ-10 книг':  # arg - List[Order]
+        #топ - 10 книг: книга, рейтинг, кол - во
+        headings = ['Автор', 'Название', 'Рейтинг', '#']
+        #data =
+    else:
+        raise ValueError(f"Unexpected name_col = {name_col}")
+    col_widths = [widths[head] for head in headings]
+    table = sg.Table(values=data, headings=headings,
+                     justification=justification,
+                     num_rows=num_rows,
+                     auto_size_columns=False,
+                     font=DEFAULT_FONT,
+                     col_widths=col_widths)
+    col_assort = [[sg.Text(name_col, justification='c', font=DEFAULT_FONT, size=(sum(col_widths), 1))], [table]]
+    return col_assort
+
+window = 1
 while True:
-    event, val = window1.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks Exit
-        break
-    if event == 'Start':
-        start_assort = [int(x) for x in val[cnt_parameters:]]
-        delivery_range = val[2].split('-')
-        delivery_range = tuple([int(delivery_range[0]), int(delivery_range[1])])
-        experiment = Experiment(simulation_period=int(val[0]), simulation_step=int(val[1]), books=books,
-                                start_assort=start_assort, delivery_range=delivery_range,
-                                minimum_cnt_for_order=int(val[5]), margin=int(val[3]), margin_new_book=int(val[4]))
-        experiment.start()
-        cols_books = [sg.Column([[] for book in books]) ]
-        cols_window2 = []
-        cols_window2.append([[sg.Text('Ассортимент')],
-                             ])
-        #window1.hide()
-        #layout2 =
+    if window == 1:
+        event, val = window1.read()
+        if event == sg.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks Exit
+            break
+        elif event == 'Start':
+            window = 2
+            start_assort = [int(x) for x in val[cnt_parameters:]]
+            delivery_range = val[2].split('-')
+            delivery_range = tuple([int(delivery_range[0]), int(delivery_range[1])])
+            experiment = Experiment(simulation_period=int(val[0]), simulation_step=int(val[1]), books=books,
+                                    start_assort=start_assort, delivery_range=delivery_range,
+                                    minimum_cnt_for_order=int(val[5]), margin=int(val[3]), margin_new_book=int(val[4]))
+            experiment.start()
+            window1.hide()
+    else:  # i.e. window == 2
+        col_assort = create_col('Ассортимент', experiment.book_shop.books)
+        col_orders = create_col('Текущие заказы', experiment.book_shop.orders)
+        col_stat = create_col('Продажи', experiment.sell_statistic)
+        col_done_ord_publ = create_col('Выполн. заявки в издат.', experiment.done_orders_publishing)
+        col_done_ord = create_col('Выполн. заказы', experiment.done_orders)
+        #col_top10_book = create_col('Топ-10 книг', )
+        layout2 = [[sg.Column(col_assort), sg.Column(col_orders), sg.Column(col_stat)],
+                   [sg.Column(col_done_ord_publ), sg.Column(col_done_ord)],
+                   [sg.Button('Stop', font=DEFAULT_FONT), sg.Button('Exit', font=DEFAULT_FONT)]]
+        window2 = sg.Window('Book Shop', layout2)
+
+        event, val = window2.read()
+        if event == 'Stop':
+            window = 1
+            window2.hide()
+            window1.un_hide()
+        elif event == sg.WIN_CLOSED or event == 'Exit':
+            break
 
 window1.close()
